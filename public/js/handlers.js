02264242472be31ae4aff9cbf3c45b810e783172
Rikoko581 <dor.globale@gmail.com>
2025-09-07 22:19:45 +0300

    Fix mobile menu - remove duplicate click event listener causing toggle conflict

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -308,33 +308,6 @@ function setupMobileMenu() {
         });
         observer.observe(mobileMenu, { attributes: true, attributeFilter: ['class'] });
         
-        menuBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            
-            const isHidden = mobileMenu.classList.contains('hidden');
-            
-            if (isHidden) {
-                // Show menu
-                mobileMenu.classList.remove('hidden');
-                menuBtn.setAttribute('aria-expanded', 'true');
-                menuBtn.innerHTML = `
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                    </svg>
-                `;
-            } else {
-                // Hide menu
-                mobileMenu.classList.add('hidden');
-                menuBtn.setAttribute('aria-expanded', 'false');
-                menuBtn.innerHTML = `
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                    </svg>
-                `;
-            }
-        });
-        
         // Add touch event support for better mobile interaction
         menuBtn.addEventListener('touchstart', (e) => {
             e.preventDefault();
